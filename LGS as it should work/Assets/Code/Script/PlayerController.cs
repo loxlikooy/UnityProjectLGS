3eb06018ab234becf8753c8e457aab328a50d13d
loxlikooy <58352668+loxlikooy@users.noreply.github.com>
2023-10-19 22:28:01 +0600

    normalized vector and camera movement
    нормализовал движение игрока, установил барьеры на край карты и изменил движение камеры за игроком сделав ее более плавной

--- LGS as it should work/Assets/Code/Script/PlayerController.cs
+++ LGS as it should work/Assets/Code/Script/PlayerController.cs
@@ -1,75 +1,71 @@
 using System.Collections.Generic;
-using Code.Script;
 using UnityEngine;
 
-// Требуется компонент InputHandler для работы
+// Assume Code.Script namespace contains InputHandler
+using Code.Script;
+using UnityEngine.Serialization;
+
+// Ensure the GameObject has an InputHandler component
 [RequireComponent(typeof(InputHandler))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float _moveSpeed = 1f; // Скорость движения игрока
+    [FormerlySerializedAs("_moveSpeed")] [SerializeField]
+    private float moveSpeed = 1f; 
 
-    private const float CollisionOffset = 0.001f; // Отступ для предотвращения застревания
+    private const float CollisionOffset = 0.001f; 
 
-    [SerializeField]
-    private ContactFilter2D _movementFilter; // Фильтр для определения объектов, с которыми игрок может столкнуться
+    [FormerlySerializedAs("_movementFilter")] [SerializeField]
+    private ContactFilter2D movementFilter; 
 
-    private Vector2 _movementInput; // Направление движения игрока
-    private Rigidbody2D _rigidbody2D; // Ссылка на Rigidbody2D компонент игрока
-    private List<RaycastHit2D> _castCollisions = new List<RaycastHit2D>(); // Список для хранения результатов лучевого просеивания
+    private Vector2 _movementInput; 
+    private Rigidbody2D _rigidbody2D; 
+    private List<RaycastHit2D> _castCollisions = new List<RaycastHit2D>();
 
-    private InputHandler _inputHandler; // Ссылка на компонент обработки ввода
+    private InputHandler _inputHandler; 
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _inputHandler = GetComponent<InputHandler>();
         
-        // Подписываемся на событие получения данных о движении
         _inputHandler.OnMoveInputReceived += HandleMoveInput;
     }
 
     private void FixedUpdate()
     {
-        // Если нет направления движения, прекращаем выполнение функции
         if (_movementInput == Vector2.zero) return;
 
-        // Попытка движения игрока
         if (!AttemptMove(_movementInput) && 
             !AttemptMove(new Vector2(_movementInput.x, 0)) && 
             !AttemptMove(new Vector2(0, _movementInput.y)))
         {
-            // Обработка ситуации, когда игрок не может двигаться ни в каком направлении
+            // Add logic here if the player can't move in any direction
         }
     }
 
-    // Метод попытки движения игрока в заданном направлении
     private bool AttemptMove(Vector2 moveDirection)
     {
         int collisionCount = CastAgainstCollidables(moveDirection);
 
-        // Если нет препятствий, двигаем игрока
         if (collisionCount == 0)
         {
-            _rigidbody2D.MovePosition(_rigidbody2D.position + moveDirection * _moveSpeed * Time.deltaTime);
+            _rigidbody2D.MovePosition( _rigidbody2D.position + (moveDirection * Time.deltaTime));
             return true;
         }
 
-        return false; // Возвращаем false, если есть препятствия
+        return false;
     }
 
-    // Метод для определения столкновений в заданном направлении
     private int CastAgainstCollidables(Vector2 moveDirection)
     {
         return _rigidbody2D.Cast(
             moveDirection,
-            _movementFilter,
+            movementFilter,
             _castCollisions,
-            _moveSpeed * Time.fixedDeltaTime + CollisionOffset
+            moveSpeed * Time.fixedDeltaTime + CollisionOffset
         );
     }
 
-    // Метод обработки данных о направлении движения
     private void HandleMoveInput(Vector2 moveInput)
     {
         _movementInput = moveInput;

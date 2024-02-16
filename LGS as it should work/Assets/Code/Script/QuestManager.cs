b027265a265fa440e87459d6ceebaf88e6f991a3
loxlikooy <58352668+loxlikooy@users.noreply.github.com>
2024-02-16 10:49:21 +0600

    BugFix
    fixed bug

--- LGS as it should work/Assets/Code/Script/QuestManager.cs
+++ LGS as it should work/Assets/Code/Script/QuestManager.cs
@@ -17,7 +17,6 @@ public class QuestManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {

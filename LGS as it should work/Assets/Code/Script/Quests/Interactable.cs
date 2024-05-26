98caef5991fb48fff1b1a2798f89958c86d00a7f
loxlikooy <58352668+loxlikooy@users.noreply.github.com>
2024-05-27 00:47:13 +0500

    interact
    interact

---
+++ LGS as it should work/Assets/Code/Script/Quests/Interactable.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using TMPro;
+
+public class Interactable : MonoBehaviour
+{
+    public string interactionKey = "E";
+    public string interactionTextObjectName = "Interact";
+
+    private TextMeshProUGUI interactionText;
+
+    private void Start()
+    {
+        GameObject interactionTextObject = GameObject.Find(interactionTextObjectName);
+
+        if (interactionTextObject != null)
+        {
+            interactionText = interactionTextObject.GetComponent<TextMeshProUGUI>();
+
+            if (interactionText != null)
+            {
+                interactionText.text = "";
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+                interactionText.text = $"To interact press {interactionKey}";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Очищаем текст взаимодействия при выходе из триггера
+                interactionText.text = "";
+        }
+    }
+}
